e3fbcf82e0447a272fcfad9bcd72640289bbd99e
Steven Van Acker <steven@singularity.be>
2024-12-07 21:37:40 +0100

    simplify

--- install.sh
+++ install.sh
@@ -3,24 +3,9 @@
 BW_ACCOUNT=$(cat ~/.myinstaller/bw-email.txt)
 SESSION_FILE=~/.bw_session
 
-# If the session file exists, read the session token from it
-if [ -f "$SESSION_FILE" ]; then
-    echo "Using existing session key..."
-    export BW_SESSION=$(cat "$SESSION_FILE")
-else
-    # If the session file doesn't exist, get a new session token
-    if bw login --check;
-    then
-        echo "Unlocking vault..."
-        export BW_SESSION=$(bw unlock --raw)
-    else
-        echo "Logging in..."
-        export BW_SESSION=$(bw login $BW_ACCOUNT --raw)
-    fi
-
-    echo "$BW_SESSION" > "$SESSION_FILE"
-    chmod go= "$SESSION_FILE"
-fi
+# ensure we are logged in and get the session token
+~/bin/bw-login-and-unlock
+export BW_SESSION=$(cat "$SESSION_FILE")
 
 pushd wifi
 ./install.sh
